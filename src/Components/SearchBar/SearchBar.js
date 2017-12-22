b4b745edf41b831dc63f4bf2cc3bc6abe705c6da
tsblessing <32779236+tsblessing@users.noreply.github.com>
2017-12-22 11:15:25 -0500

    Add files via upload

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -10,6 +10,7 @@ class SearchBar extends React.Component {
 
   search (term) {
     this.props.onSearch(this.state.term)
+    }
   }
 
   handleTermChange (event) {
--- src/Util/Spotify.js
+++ src/Util/Spotify.js
@@ -25,7 +25,7 @@ const Spotify = {
   },
 
   search (term) {
-    const accessToken = Spotify.getAccessToken
+    const accessToken = Spotify.getAccessToken();
     return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}` , {
       headers: {Authorization: `Bearer ${accessToken}`}
     }).then(response => {

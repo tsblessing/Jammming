6340a11f0cea482e06b9c1f39dd5ee59160662bc
tsblessing <32779236+tsblessing@users.noreply.github.com>
2017-12-23 12:28:38 -0500

    Add files via upload

---
+++ Tracklist.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Tracklist.css';
+import Track from '../Track/Track'
+
+class Tracklist extends React.Component {
+
+  render () {
+    return (
+      <div className="TrackList">
+        {this.props.tracks.map(track => {
+          return <Track track={track} key={track.id} onAdd = {this.props.onAdd} isRemoval = {this.props.isRemoval} onRemove = {this.props.onRemove}/>
+        })}
+      </div>
+    )
+  }
+}
+
+
+export default Tracklist;
